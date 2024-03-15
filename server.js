2b7ae8da6f80fb8888330cf6bc1627d134cad9e7
MarceloSilva0001 <117323231+MarceloSilva0001@users.noreply.github.com>
2024-03-14 21:19:42 -0300

    Api mesas: refatoração, atualização ok

--- server.js
+++ server.js
@@ -28,57 +28,64 @@ app.post('/api/v1/actions', async (req, res) => {
   const taxa = req.body.taxa;
 
   try {
-    if (select === "table") { 
-      if (select2 == 2) {
-        // Função para atualizar uma mesa
-        async function updateTable(tableNumber, newData) {
-          try {
-            const response = await axios.put(`https://developers.abrahao.com.br/api/v1/table/${tableNumber}`, newData, {
-              headers: {
-                'Authorization': `Bearer ${token}`,
-                'Content-Type': 'application/json' // Especifica que os dados são JSON
-              }
-            });
-            console.log(`Mesa ${tableNumber} atualizada com sucesso:`, response.data);
-          } catch (error) {
-            console.error(`Erro ao atualizar mesa ${tableNumber}:`, error.response.data);
+      switch((select)){
+        case "table":
+          console.log("Opção table selecionada");
+          switch(select2) {
+            case 1:// Cadastrar Mesa
+              console.log("Opção 1 selecionada Cadastrar Mesas");
+              
+              break;
+            case 2:// Atualizar Mesa
+              console.log("Opção 2 selecionada Atualizar Mesas");
+              
+                async function updateTable(tableNumber, newData) {
+                  try {
+                    const response = await axios.put(`https://developers.abrahao.com.br/api/v1/table/${tableNumber}`, newData, {
+                      headers: {
+                        'Authorization': `Bearer ${token}`,
+                        'Content-Type': 'application/json' // Especifica que os dados são JSON
+                      }
+                    });
+                    console.log(`Mesa ${tableNumber} atualizada com sucesso:`, response.data);
+                  } catch (error) {
+                    console.error(`Erro ao atualizar mesa ${tableNumber}:`, error.response.data);
+                  }
+                }
+        
+                // Itera sobre as mesas e atualiza cada uma
+                for (let i = initial; i <= final; i++) {
+                  const newData = {
+                    name: nome, // Novo nome da mesa
+                    service_percentage: taxa // Nova porcentagem de serviço
+                  };
+        
+                  await updateTable(i, newData);
+                }
+                res.json({ success: true, message: `Ações concluídas com sucesso para ${select}s ${initial} até ${final}!` });
+              
+              break;
+            case 3:// Excluir Mesa
+              console.log("Opção 3 selecionada Excluir Mesas");
+              
+              break;
+            default:
+              console.log("Opção inválida selecionada");
+              res.json({ success: true, message: `Algo deu errado, tente novamente :(` });
           }
-        }
-
-        // Itera sobre as mesas e atualiza cada uma
-        for (let i = initial; i <= final; i++) {
-          const newData = {
-            name: nome, // Novo nome da mesa
-            service_percentage: taxa // Nova porcentagem de serviço
-          };
-
-          await updateTable(i, newData);
-        }
-        res.json({ success: true, message: `Ações concluídas com sucesso para ${select}s ${initial} até ${final}!` });
-      } else {
-        res.json({ success: true, message: `Algo deu errado, tente novamente` });
+          break;
+        case 2:
+          console.log("Opção 2 selecionada");
+          // Faça algo para a opção 2
+          break;
+        case 3:
+          console.log("Opção 3 selecionada");
+          // Faça algo para a opção 3
+          break;
+        default:
+          console.log("Opção inválida selecionada");
+    // Faça algo para o caso em que nenhuma opção correspondente é encontrada
       }
-      /*
-        if (select2 == 3) {
-        for (let i = initial; i <= final; i++) {
-          try {
-            await axios.delete(`https://developers.abrahao.com.br/api/v1/table/${i}`, null, {
-              headers: {
-                'Authorization': `Bearer ${token}`
-              }
-            });
-          } catch (error) {
-            console.error(`Erro ao processar ação para ${select}s ${initial} até ${final}:`, error.response.data);
-            // Continuar para a próxima iteração mesmo se houver um erro
-            continue;
-          }
-        }
-        res.json({ success: true, message: `Ações concluídas com sucesso para ${select}s ${initial} até ${final}!` });
-      }else {
-        res.json({ success: true, message: `Algo deu errado, tente novamente` });
-      }
-      */ 
-    }
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: `Erro ao realizar ações: ${error.message}` });
